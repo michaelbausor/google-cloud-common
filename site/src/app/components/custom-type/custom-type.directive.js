8ec865f74dda8ffb9687313016e5a564908b8456
Chris Smith <quartzmo@gmail.com>
2016-02-11 14:30:57 -0700

    Add ruby support to customType

--- site/src/app/components/custom-type/custom-type.directive.js
+++ site/src/app/components/custom-type/custom-type.directive.js
@@ -7,28 +7,33 @@
 
   /** @ngInject */
   function customType($state, manifest) {
-    var converter = {
-      node: function(customType) {
-        var parts = customType.split('#');
-        var stateName = 'docs.service';
-        var stateParams = { serviceId: parts[0] };
-        var stateOptions = { inherit: false };
 
-        if (parts.length > 1) {
-          stateParams.method = parts[1];
-        }
+    var convertToServicePath = function(customType) {
+      var parts = customType.split('#');
+      var stateName = 'docs.service';
+      var stateParams = { serviceId: parts[0] };
+      var stateOptions = { inherit: false };
 
-        return $state.href(stateName, stateParams, stateOptions);
+      if (parts.length > 1) {
+        stateParams.method = parts[1];
       }
+
+      return $state.href(stateName, stateParams, stateOptions);
+    };
+
+    var converter = {
+      node: convertToServicePath,
+      ruby: convertToServicePath
     }[manifest.lang];
 
     return {
       restrict: 'A',
       link: function (scope, elem, attrs) {
         var customType = attrs.customType;
-
-        elem.html(customType)
-          .addClass('skip-external-link')
+        if (elem.html().length === 0) {
+          elem.html(customType) // Set path as text if no text in element
+        }
+        elem.addClass('skip-external-link')
           .attr('href', converter(customType.replace('[]', '')));
       }
     };
