8748d273fcdaabb645097e8e660a32830bd60918
Dave Gramlich <callmehiphop@gmail.com>
2016-02-12 17:55:11 -0500

    docs: preserve version on custom type links

--- site/src/app/components/custom-type/custom-type.directive.js
+++ site/src/app/components/custom-type/custom-type.directive.js
@@ -7,11 +7,13 @@
 
   /** @ngInject */
   function customType($state, manifest) {
-
     var convertToServicePath = function(customType) {
       var parts = customType.split('#');
       var stateName = 'docs.service';
-      var stateParams = { serviceId: parts[0] };
+      var stateParams = {
+        serviceId: parts[0],
+        version: $state.params.version
+      };
       var stateOptions = { inherit: false };
 
       if (parts.length > 1) {
@@ -31,7 +33,7 @@
       link: function (scope, elem, attrs) {
         var customType = attrs.customType;
         if (elem.html().length === 0) {
-          elem.html(customType) // Set path as text if no text in element
+          elem.html(customType); // Set path as text if no text in element
         }
         elem.addClass('skip-external-link')
           .attr('href', converter(customType.replace('[]', '')));
