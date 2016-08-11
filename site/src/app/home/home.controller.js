ea385c8f50280a3a31dab6333ff10bbdb404cc13
Dave Gramlich <callmehiphop@gmail.com>
2016-08-10 21:28:00 -0400

    added support for overview key

--- site/src/app/home/home.controller.js
+++ site/src/app/home/home.controller.js
@@ -6,11 +6,25 @@
     .controller('HomeCtrl', HomeCtrl);
 
   /** @ngInject */
-  function HomeCtrl(manifest, latestRelease) {
+  function HomeCtrl(manifest, latestRelease, util) {
     var home = this;
 
     home.contentUrl = [manifest.content, manifest.home].join('/');
     home.latestRelease = latestRelease;
+
+    if (!manifest.modules) {
+      return;
+    }
+
+    var module = util.findWhere(manifest.modules, {
+      id: manifest.defaultModule
+    });
+
+    home.module = {
+      name: module.id,
+      version: module.versions[0],
+      service: module.defaultService
+    };
   }
 
 }());
